d20a58728679907aefadc3b3e23d2679042a3432
Chad Rotruck <crotruck@gmail.com>
2023-02-03 02:14:48 +0000

    done

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -27,6 +27,10 @@ public class GameService {
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
-        return false;
+        if (secretNumber == guessNumber){
+            return true;
+        } else { 
+            return false;
+        }
     }
 }
